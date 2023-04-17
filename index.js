a05d428eeae5b35aaf75bfbd11c78106543d9795
JackSSads <fotosjackson2018@hotmail.com>
2023-04-17 15:59:42 -0300

    Corrigindo String de retorno

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on(message('text'), async (ctx) => {
 
             // Redirecionando todas as buscas para o meu Chat contendo nome do usuário, pergunta e resposta obtida
             if (ctx.message.chat.id != process.env.MY_CHAT_ID){
-                await ctx.telegram.sendMessage(process.env.MY_CHAT_ID, `Usuário: ${ctx.message.chat.first_name}\nChat ID: ${ctx.message.chat.id}\n\nPergunta: ${message}\nestposta: ${answer}`);
+                await ctx.telegram.sendMessage(process.env.MY_CHAT_ID, `Usuário: ${ctx.message.chat.first_name}\nChat ID: ${ctx.message.chat.id}\n\nPergunta: ${message}\nRestposta: ${answer}`);
             };
         } catch (error) {
             // Caso ocorra algum erro na requisição, envia uma mensagem de erro para o usuário

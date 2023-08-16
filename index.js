93db42c3de1c6f48f336503faa657971988588fb
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2023-08-15 21:11:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on(message('text'), async (ctx) => {
 
             if (ctx.message.chat.id != process.env.MY_CHAT_ID) {
                 await ctx.telegram.sendMessage(
-                    process.env.MY_CHAT_ID, `Usuário: ${ctx.message.chat.first_name}\nChat ID: ${ctx.message.chat.id}\n\nPergunta:\n${message}\n\nRestposta:\n${answer}`
+                    process.env.MY_CHAT_ID, `Usuário: ${ctx.message.chat.first_name}\nChat ID: ${ctx.message.chat.id}\n\nPergunta:\n${message}\n\nResposta:\n${answer}`
                 );
             };
         } catch (error) {

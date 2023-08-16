86ef2dd5ad2f7e0a4a1dcb7b58a68d5cf28ce195
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2023-08-15 21:24:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const { Telegraf } = require('telegraf');
-
 const { message } = require('telegraf/filters');
-
 require('dotenv').config();
 
 const app = require("./api/server");
 
-const PORT = process.env.PORT || 3000;
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears('/start', (ctx) => {
-
     const user = ctx.message.chat.first_name;
-
     ctx.reply(`Olá, ${user}! Seja bem-vindo!`);
 });
 
 bot.on(message('text'), async (ctx) => {
 
     const message = ctx.message.text;
-
+    
     const getQuestion = async () => {
         try {
-            const response = await fetch(`http://localhost:${PORT}/${message}`)
+            const response = await fetch(`http://localhost:${PORT}/${message}`);
 
             const json = await response.json();
 
@@ -32,14 +26,18 @@ bot.on(message('text'), async (ctx) => {
             await ctx.telegram.sendMessage(ctx.message.chat.id, answer);
 
             if (ctx.message.chat.id != process.env.MY_CHAT_ID) {
+
+                const fristName = ctx.message.chat.first_name;
+                const lastName = ctx.message.chat.last_name;
+
                 await ctx.telegram.sendMessage(
-                    process.env.MY_CHAT_ID, `Usuário: ${ctx.message.chat.first_name}\nChat ID: ${ctx.message.chat.id}\n\nPergunta:\n${message}\n\nResposta:\n${answer}`
+                    process.env.MY_CHAT_ID, `Usuário: ${fristName} ${lastName}\nChat ID: ${ctx.message.chat.id}\n\nPergunta:\n${message}\n\nRestposta:\n${answer}`
                 );
             };
         } catch (error) {
             console.log(error);
 
-            const errorMessage = `Hum... Algo deu errado.\nTenta de novo;`
+            const errorMessage = `Hum... Algo deu errado.\nTenta de novo;`;
 
             await ctx.telegram.sendMessage(ctx.message.chat.id, errorMessage);
         };
@@ -50,6 +48,6 @@ bot.on(message('text'), async (ctx) => {
 
 bot.launch();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("API ligada!")
+app.listen(process.env.PORT, () => {
+    console.log("API ligada!");
 });
